9a29ba044bba1c21528c2ef66f0d1053535a0f07
Proizaac <762769@student.dusd.net>
2025-11-13 11:40:14 -0800

    game

--- P4Trujillo2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ P4Trujillo2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -5,32 +5,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerController1 : MonoBehaviour
 {
+    public InputAction MoveAction;
     public InputAction LeftAction;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-       
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-        Debug.Log(horizontal);
-
-
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
+
+
 }
