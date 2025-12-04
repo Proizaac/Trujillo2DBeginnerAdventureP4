895b6353825f966a594d05800478bcb3c51e9a88
Proizaac <762769@student.dusd.net>
2025-12-04 05:48:13 -0800

    fixed damage zones and started adding animations to the enemy

--- P4Trujillo2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ P4Trujillo2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -44,9 +44,7 @@ public class PlayerController1 : MonoBehaviour
         {
             damageCooldown -= Time.deltaTime;
             if (damageCooldown < 0)
-            {
                 isInvincible = false;
-            }
         }
     }
 
@@ -64,16 +62,15 @@ public class PlayerController1 : MonoBehaviour
         if (amount < 0)
         {
             if (isInvincible)
-            {
                 return;
-            }
+
             isInvincible = true;
             damageCooldown = timeInvincible;
         }
 
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
     }
 
 
